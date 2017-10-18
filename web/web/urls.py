175baf5f4308b46ef9ed367e28f4189bd60448d1
Kevin <kwa6nf@virginia.edu>
2017-10-18 10:37:44 -0400

    added sorting popular users feature

--- web/web/urls.py
+++ web/web/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
     url(r'^login/$', views.login, name='login'),
     url(r'^logout/$', views.logout, name='logout'),
     url(r'^createListing/$', views.createListing, name='createListing'),
+    url(r'^popularUsers/$', views.getPopularUsers, name='getPopularUsers'),
+
 ]
