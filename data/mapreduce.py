0fc64513a6f861a910b2fcec4e41e239b3d2dea7
root <kr3ad@virginia.edu>
2017-12-03 19:38:01 -0500

    inserting into database woo party

--- data/mapreduce.py
+++ data/mapreduce.py
@@ -1,4 +1,11 @@
 from pyspark import SparkContext
+import MySQLdb
+
+
+db = MySQLdb.connect(host='db', user='www', passwd='$3cureUS', db='cs4501')
+cur = db.cursor()
+cur.execute('delete from ItemManager_recommendation')
+
 
 sc = SparkContext("spark://spark-master:7077", "PopularItems")
 
@@ -11,23 +18,41 @@ pairs2 = pairs.groupByKey()   # (User,list of item ids)
 
 cartesian = []
 for user, items in pairs2.collect():
-	product = sc.parallelize(items).cartesian(sc.parallelize(items)).filter(lambda x: x[0] < x[1]).distinct()
+	product = sc.parallelize(items).cartesian(sc.parallelize(items)).filter(lambda x: x[0] != x[1]).distinct()
 	for c in product.collect():
 		t = (user,c)
 		cartesian.append(t)
-
-print("cartesian %s" % cartesian)
 
 cartesian = sc.parallelize(cartesian)
 
 
 pages = cartesian.map(lambda pair: (pair[1], pair[0]))
+
+output = {}
 for tup, peeps in pages.groupByKey().collect():
-    for thing in peeps:
-    	print ((tup, thing))
-print ("Popular items done")
+	tup = tuple(tup)
+	output[tup] = len(peeps)
+
+recom_dict = {}
+for page, count in output.items():
+	if count >= 3:
+		try:
+			recom_dict[page[0]] += ' '+str(page[1])
+		except KeyError, e:
+			recom_dict[page[0]] = str(page[1])
 
 
+for key, value in recom_dict.items():
+	val = value.encode('UTF-8')
+	k = int(key)
+	query = 'INSERT INTO ItemManager_recommendation (item_id, recommended_items) VALUES (%d, \'%s\');' % (k, val)
+	cur.execute(query)
+	print(key, value, 'inserted?')
+
+db.commit()
+# filtered = sc.parallelize(output)
+#
+# print("filtered:",filtered.collect())
 
 # print("pages", pages.groupByKey().collect())
 
@@ -38,5 +63,6 @@ print ("Popular items done")
 # for page_id, count in output:
 #     print ("page_id %s count %d" % (page_id, count))
 # print ("Popular items done")
+
 
 sc.stop()
